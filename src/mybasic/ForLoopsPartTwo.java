5cfee99f0e1c5db05d840ea38444de6ffd5a0dfa
hurayra <khanjada.hurayra.qa@gmail.com>
2019-05-25 09:52:12 -0400

    Loops commit

---
+++ src/mybasic/ForLoopsPartTwo.java
@@ -0,0 +1,16 @@
+package mybasic;
+
+public class ForLoopsPartTwo {
+
+    public static void main(String[]args){
+
+     String name[]={"Rony","Ashraf","Hasan","Dilruba"};
+     for (String i:name) {
+
+         System.out.println("we r doing QA - "+i);
+
+     }
+
+
+    }
+}
