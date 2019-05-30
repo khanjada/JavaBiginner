edba790323cbec1267c79b22b9e724a8b78eb6c4
hurayra <khanjada.hurayra.qa@gmail.com>
2019-05-30 18:55:19 -0400

    dowhileloop commit

---
+++ src/mybasic/DoWhileLoop.java
@@ -0,0 +1,19 @@
+package mybasic;
+
+public class DoWhileLoop {
+
+    public static void main(String[] args) {
+
+        int i=20;
+
+        do {
+            System.out.println("result="+i);
+            i++;
+        }
+        while(i!=30);
+    }
+
+
+
+
+}
