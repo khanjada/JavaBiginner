2ed863860d4aa29e975ced878ef063d9e7dd706e
hurayra <khanjada.hurayra.qa@gmail.com>
2019-06-07 21:28:48 -0400

    whileloop commit

---
+++ src/mybasic/WhileLoop.java
@@ -0,0 +1,13 @@
+package mybasic;
+
+public class WhileLoop {
+
+    public static void main(String[] args) {
+        int i=30;
+        while (i<40){
+
+            System.out.println("result="+i);
+            i++;
+        }
+    }
+}
